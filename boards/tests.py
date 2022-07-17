c1efdf1ae69099efa57d5bbb340089ae3ed729a4
Mbani <mouad-mb@outlook.com>
2022-07-17 17:09:15 +0100

    write unit test

--- boards/tests.py
+++ boards/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
 # Create your tests here.
+
+class homeTest(TestCase):
+	def test_home_status_code(self):
+		print("")
+		response = self.client.get('/boards', follow=True)
+		self.assertEqual(response.status_code, 200)
