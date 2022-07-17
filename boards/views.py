fd48f56b86effa263d5e08849708f9623ac41155
Mbani <mouad-mb@outlook.com>
2022-07-17 17:09:47 +0100

    setup routes/views

--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from . import models
 # Create your views here.
+def all(request):
+	boards = models.Board.objects.all()
+	return HttpResponse(boards)
